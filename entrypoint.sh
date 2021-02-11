2f454b42b0c8c096cad227a4162b91e644a4c5af
Jason Fox <jason.fox@fiware.org>
2021-02-12 00:28:30 +0100

    Switch order

--- entrypoint.sh
+++ entrypoint.sh
@@ -68,9 +68,9 @@ else
 		cd /tmp
 		git clone "${GITHUB_SERVER_URL}"/"${GITHUB_REPOSITORY}"
 		cd "${GITHUB_REPOSITORY##*/}"
+		cp /opt/app/plugins/"${PLUGIN}"/*.xml .
 		cp /github/workspace/coverage.xml coverage.xml
 		cp /opt/app/plugins/fox.jason.unit-test/resource/pom.xml pom.xml
-		cp /opt/app/plugins/"${PLUGIN}"/*.xml .
 		/opt/apache-maven-3.6.3/bin/mvn -q org.eluder.coveralls:coveralls-maven-plugin:report -DrepoToken="${COVERALLS_TOKEN}"
 	fi
 fi
